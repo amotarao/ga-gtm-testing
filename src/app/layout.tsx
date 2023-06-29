4d021c09f9383c66f3ab9702571bf1429c76bdea
Amon Sawamura <amotarao@gmail.com>
2023-06-29 19:40:54 +0900

    add gtm tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,15 +22,12 @@ export default function RootLayout({
           <Link href="/about">About</Link>
         </nav>
         {children}
+        <Script id="gtm-push" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5CQ5LWH');` }} />
       </body>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V61XENS0YQ"></Script>
-      <Script id="gtag-push" dangerouslySetInnerHTML={{ 
-        __html: `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-V61XENS0YQ');`
-      }} />
     </html>
   )
 }
